492c1b502d76b5d5f383ec8dffde35a703f611cc
Mike <mikhail.yarulin@jadexconsulting.com>
2026-02-08 21:18:03 +0200

    load channels from assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -55,6 +56,8 @@ dependencies {
     // koin
     implementation(libs.bundles.di)
 
+    // Tools
+    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/solarrage/androidtest/data/source/ChannelLocalDataSourceImpl.kt
+++ app/src/main/java/com/solarrage/androidtest/data/source/ChannelLocalDataSourceImpl.kt
@@ -2,11 +2,37 @@ package com.solarrage.androidtest.data.source
 
 import android.content.Context
 import com.solarrage.androidtest.data.model.ChannelDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
 
 class ChannelLocalDataSourceImpl(
     private val context: Context
 ) : ChannelLocalDataSource {
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    private var cache: List<ChannelDto>? = null
+
     override suspend fun loadChannels(): List<ChannelDto> {
-        TODO("Not yet implemented")
+        return cache ?: loadAndCache()
+    }
+
+    private suspend fun loadAndCache(): List<ChannelDto> =
+        withContext(Dispatchers.IO) {
+            val text = context.assets
+                .open(FILE_NAME)
+                .bufferedReader()
+                .use { it.readText() }
+
+            val parsed = json.decodeFromString<List<ChannelDto>>(text)
+            cache = parsed
+            return@withContext parsed
+        }
+
+    private companion object {
+        const val FILE_NAME = "channels.json"
     }
 }
